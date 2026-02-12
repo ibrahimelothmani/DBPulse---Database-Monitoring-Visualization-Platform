d33844c97739f0e4639f1a78c3835a9d7029035a
Ibrahim El Othmani <141041774+ibrahimelothmani@users.noreply.github.com>
2026-02-12 22:27:22 +0100

    chore: Update PostgreSQL connection details, remove spring-boot-docker-compose dependency, and add application startup logging.

--- src/main/java/com/ibrahim/DBPulse/DbPulseApplication.java
+++ src/main/java/com/ibrahim/DBPulse/DbPulseApplication.java
@@ -1,13 +1,15 @@
 package com.ibrahim.DBPulse;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class DbPulseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbPulseApplication.class, args);
+		log.info("DBPulse application started successfully.");
 	}
-
 }
